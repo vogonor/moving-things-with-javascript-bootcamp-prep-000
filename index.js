718362f9588a330e3a93d5782255c067573f92ac
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-26 07:13:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,4 +31,11 @@ function moveDodgerLeft() {
   }
 }
 
-
+function moveDodgerRight() {
+  var RightNumbers = dodger.style.moveDodgerRight.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
