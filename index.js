d77da902127e346aa2d2a78b035f86f2b54afd95
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-26 07:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ function moveDodgerRight() {
   var right = parseInt(rightNumbers, 10)
  
   if (right === 0) {
-    dodger.style.right = `${right - 1}px`
+    dodger.style.right = `${right + 1}px`
+  } else {
+    dodger.style.right = `${right + 1}px`
   }
 }
