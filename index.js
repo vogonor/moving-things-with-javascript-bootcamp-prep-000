b75c2b5a3126fefcbb57dca21627e2fa85e5fcc1
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-26 02:02:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+var dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#000000"
+dodger.style.backgroundColor = '#FF69B4'
+dodger.style.left // "180px"
+dodger.style.bottom // "0px"
+dodger.style.bottom = '100px'
+dodger.style.bottom = '0px'
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+})
