5995a36b3b1789a7e2f782521a62d2694abe3675
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-26 07:05:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,11 @@ document.addEventListener('keydown', function(e) {
 
 var dodger = document.getElementById('dodger')
 
-
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
